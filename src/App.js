3887878d81bb82eb5dff6f5b9561c0e318f1e1f3
MrCookiez <theodorosvragkos@gmail.com>
2019-01-19 15:51:25 +0100

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// import { Navbar, Hero, HomeContent, Footer } from './components';
+import './assets/css/main.css';
+
+const newRoute = () => {
+  return() {
+    <div>
+      <p>HEY</p>
+    </div>
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         {/* <img src={js} alt="logo" /> */}
+         {/* <Navbar />
+         <Hero />
+         <HomeContent />
+         <Footer /> */}
+         <BrowserRouter>
+            <Route path="new" component="" />
+         </BrowserRouter>
       </div>
     );
   }
---
+++ src/assets/images/logos/index.js
@@ -0,0 +1,5 @@
+import html5 from './html5.svg';
+import css3 from './css3.svg';
+import js from './js.svg';
+import php from './php.svg';
+import mysql from './mysql.svg';
